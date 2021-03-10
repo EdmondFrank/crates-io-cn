1861533ac8c474a06d53c95c07b8440e380bd8cd
EdmondFrank <Edmondfrank@hotmail.com>
2021-03-10 15:00:33 +0800

    feat(demo): first release support bos

--- src/bos/bos.rs
+++ src/bos/bos.rs
@@ -1,15 +1,12 @@
-// Modified from https://github.com/mozilla/sccache/blob/master/src/simples3/s3.rs
 use core::fmt;
-
+use std::str;
 use bytes::Bytes;
 use reqwest::Response;
 use sha2::Sha256;
 use hmac::{Hmac, NewMac, Mac};
 use super::credentials::*;
-// use chrono::{DateTime, NaiveDateTime, Utc};
-// use std::time::SystemTime;
-
-
+use url::Url;
+use md5::{Md5, Digest};
 
 #[derive(Debug, Copy, Clone)]
 #[allow(dead_code)]
@@ -21,14 +18,15 @@ pub enum BosSsl {
     No,
 }
 
-fn base_url(endpoint: &str, ssl: BosSsl) -> String {
+fn base_url(endpoint: &str, bucket_name: &str, ssl: BosSsl) -> String {
     format!(
-        "{}://{}/",
+        "{}://{}/{}",
         match ssl {
             BosSsl::Yes => "https",
             BosSsl::No => "http",
         },
-        endpoint
+        endpoint,
+        bucket_name
     )
 }
 
@@ -39,12 +37,18 @@ fn hmac(key: &[u8], data: &[u8]) -> Vec<u8> {
     hmac256.finalize().into_bytes().as_slice().to_vec()
 }
 
-fn signature(string_to_sign: &str, signing_key: &str) -> String {
-    let s = hmac(signing_key.as_bytes(), string_to_sign.as_bytes());
-    base64::encode(&s)
+fn md5_hash(content: &[u8]) -> String {
+    let mut hasher = Md5::new();
+    hasher.update(content);
+    base64::encode(hasher.finalize())
 }
 
-/// An Obs bucket.
+fn signature(string_to_sign: &str, signing_key: &str) -> String {
+    let s = hmac(signing_key.as_bytes(), string_to_sign.as_bytes());
+    base16::encode_lower(&s)
+}
+
+/// An Bos bucket.
 pub struct BosBucket {
     name: String,
     base_url: String,
@@ -60,161 +64,123 @@ impl fmt::Display for BosBucket {
 
 impl BosBucket {
     pub fn new(name: &str, endpoint: &str, ssl: BosSsl) -> BosBucket {
-        let base_url = base_url(&endpoint, ssl);
+        let base_url = base_url(&endpoint, &name, ssl);
         BosBucket {
             name: name.to_owned(),
             base_url,
-            host: format!("{}.{}", &name, &endpoint),
+            host: endpoint.to_owned(),
             client: reqwest::Client::new(),
         }
     }
 
     pub async fn put(&self, key: &str, content: Bytes, creds: &BosCredentials) -> Result<Response> {
-        use chrono::Utc;
+        use chrono::{Utc, SecondsFormat};
         use reqwest::header;
 
-        let url = format!("{}{}", self.base_url, key);
+        let url = format!("{}/{}", self.base_url, key);
         debug!("PUT {}", url);
         let request = self.client.put(&url);
 
         let content_type = "application/octet-stream";
-        let date = Utc::now().format("%a, %d %b %Y %H:%M:%S GMT").to_string();
-        let mut canonical_headers = String::new();
-        let request = if let Some(ref token) = creds.security_token() {
-            canonical_headers.push_str(format!("{}:{}\n", "x-obs-security-token", token).as_ref());
-            request.header("x-obs-security-token", token)
-        } else {
-            request
-        };
+        let date = Utc::now().to_rfc3339_opts(SecondsFormat::Secs, true);
+        let request_url = Url::parse(&url)?;
+        let md5 = md5_hash(&content);
+        let content_length = content.len();
+        let query_string = request_url.query().unwrap_or("");
+        let path = request_url.path();
 
         let auth = self.auth(
             "PUT",
+            path,
+            query_string,
+            content_length,
+            &md5,
             &date,
-            key,
-            "",
-            &canonical_headers,
             content_type,
             creds,
         );
+        debug!("auth {}", auth);
+
+
+
         let request = request
-            .header(header::DATE, date)
-            .header(header::HOST, &self.host)
-            .header(header::CONTENT_TYPE, content_type)
+            .header("Content-Md5", md5)
             .header(header::CONTENT_LENGTH, content.len())
-            .header(header::AUTHORIZATION, auth)
-            .body(content);
+            .header(header::CONTENT_TYPE, content_type)
+            .header(header::HOST, &self.host)
+            .header("X-BCE-Date", date.clone())
+            .header(header::AUTHORIZATION, auth.clone())
+            .body(content.clone());
 
         let result = request.send().await?;
         let headers = result.headers();
-        let request_id = headers
-            .get("x-obs-request-id")
+        let bce_request_id = headers
+            .get("x-bce-request-id")
             .map_or_else(|| "null", |h| h.to_str().unwrap_or("invalid"));
-        let obs_id = headers
-            .get("x-obs-id-2")
+        let bce_debug_id = headers
+            .get("x-bce-debug-id")
             .map_or_else(|| "null", |h| h.to_str().unwrap_or("invalid"));
         trace!(
-            "obs result: [{}], x-obs-request-id: {:?}, x-obs-id-2: {:?}",
+            "BOS status: [{}] x-bce-request-id: {:?}, x-bce-debug-id: {:?}",
             result.status(),
-            request_id,
-            obs_id
+            bce_request_id,
+            bce_debug_id
         );
         Ok(result)
     }
 
-    /// https://github.com/baidubce/bce-sdk-go/blob/master/auth/signer.go
-    ///
-    /// StringToSign definition:
-    /// ```text
-    /// StringToSign = HTTP-Verb + "\n"
-    /// + Content-MD5 + "\n"
-    /// + Content-Type + "\n"
-    /// + Date + "\n"
-    /// + CanonicalizedHeaders + CanonicalizedResource
-    /// ```
-    ///
-    /// CanonicalizedHeaders definition:
-    /// 1. filter all header starts with `x-obs-`, convert to lowercase
-    /// 2. sort by dictionary order
-    /// 3. append with `key:value\n`, concat duplicate key-value with `,` (example:`key:value1,value2\n`)
-    ///
-    /// Signature definition:
-    /// ```text
-    /// Signature = Base64( HMAC-SHA1( SecretAccessKeyID, UTF-8-Encoding-Of( StringToSign ) ) )
-    /// ```
-    ///
     #[allow(clippy::too_many_arguments)]
     pub(crate) fn auth(
         &self,
         verb: &str,
-        date: &str,
-        path: &str,
+        canonical_uri: &str,
+        canonical_query_string: &str,
+        content_length: usize,
         md5: &str,
-        headers: &str,
+        date: &str,
         content_type: &str,
         creds: &BosCredentials,
     ) -> String {
 
-        let string = format!(
-            "{verb}\n{md5}\n{ty}\n{date}\n{headers}{resource}",
-            verb = verb,
-            md5 = md5,
-            ty = content_type,
+        let expiration = 1800;
+        let sign_key_info = format!(
+            "bce-auth-v1/{access}/{date}/{expiration}",
+            access = creds.access(),
             date = date,
-            headers = headers,
-            resource = format!("/{}/{}", self.name, path)
+            expiration = expiration,
         );
-        let signature = signature(&string, creds.secret());
-        format!("OBS {}:{}", creds.access(), signature)
-    }
+        trace!("sign_key_info: {}", sign_key_info);
 
-    // #[allow(clippy::too_many_arguments)]
-    // pub(crate) fn sign(
-    //     &self,
-    //     credentials: &BosCredentials,
-    //     http_method: &str,
-    //     path: &str,
-    //     headers: &str,
-    //     params: &str,
-    //     timestamp: Option<i64>,
-    //     expiration_in_seconds: Option<i64>,
-    //     headers_to_sign: Box<[&str]>,
-    // ) -> String {
-    //
-    //     let date = if let Some(time) = timestamp {
-    //         let navie = NaiveDateTime::from_timestamp(time, 0);
-    //         let datetime: DateTime<Utc> = DateTime::from_utc(navie, Utc);
-    //         format!("{:?}", datetime)
-    //     } else {
-    //         let now: DateTime<Utc> = SystemTime::now().into();
-    //         let timestamp = now.timestamp();
-    //         let navie = NaiveDateTime::from_timestamp(timestamp, 0);
-    //         let datetime: DateTime<Utc> = DateTime::from_utc(navie, Utc);
-    //         format!("{:?}", datetime)
-    //     };
-    //
-    //     let expiration = if let Some(expire) = expiration_in_seconds {
-    //         expire
-    //     } else {
-    //         1800
-    //     };
-    //
-    //     let sign_key_info = format!(
-    //         "bce-auth-v1/{access}/{date}/{expiration}",
-    //         access = credentials.access(),
-    //         date = date,
-    //         expiration = expiration
-    //     );
-    //
-    //
-    //     let sign_key = signature(credentials.secret(), sign_key_info.as_str());
-    //     let canonical_uri = if path.len() == 0 {
-    //         "/".to_string()
-    //     } else {
-    //         let mut path = urlencoding::encode(path).replace("%2F", "/").to_string();
-    //         if path.starts_with("/") { path } else { path.insert_str(0, "/") }
-    //     };
-    //
-    //     let signature = signature(&string, creds.secret());
-    //     format!("OBS {}:{}", creds.access(), signature)
-    // }
+        let sign_key = signature(&sign_key_info, creds.secret());
+        trace!("sign_key: {}", sign_key);
+
+        let canonical_headers = format!(
+            "content-length:{content_length}\n\
+            content-md5:{md5}\n\
+            content-type:{ty}\n\
+            host:{host}\n\
+            x-bce-date:{date}",
+            content_length = content_length,
+            md5 = urlencoding::encode(md5),
+            ty = urlencoding::encode(content_type),
+            host = self.host,
+            date = urlencoding::encode(date),
+        );
+
+        let canonical_request = format!(
+            "{verb}\n{canonical_uri}\n{canonical_query_string}\n{canonical_headers}",
+            verb = verb,
+            canonical_uri = canonical_uri,
+            canonical_query_string = canonical_query_string,
+            canonical_headers = canonical_headers,
+        );
+
+        let signature = signature(&canonical_request, &sign_key);
+
+        trace!("signature: {}", signature);
+        let header_str = "content-length;content-md5;content-type;host;x-bce-date";
+        let final_signature = format!("{}/{}/{}", sign_key_info, header_str, signature);
+        format!("BOS auth: {}", final_signature);
+        final_signature
+    }
 }
