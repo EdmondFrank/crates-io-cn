3230cb65af1cad0a211ac30a2e7a9b372ae08fa7
EdmondFrank <Edmondfrank@hotmail.com>
2021-03-16 15:39:04 +0800

    feat(crate): support cli --sync-all

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ mod simple_obs;
 mod upyun;
 #[cfg(feature = "bos")]
 mod bos;
+#[cfg(feature = "sync")]
+use clap::{App as CliApp, Arg};
 #[cfg(all(feature = "systemd-integration", target_os = "linux"))]
 mod systemd;
 mod easy_git;
@@ -29,6 +31,9 @@ use crate::index::{Config, GitIndex};
 use helper::{Crate, CrateReq};
 use std::ops::{Add, Deref};
 use tokio::time::{Duration, Instant};
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use walkdir::{WalkDir, DirEntry};
 
 lazy_static! {
     static ref GIT_INDEX_DIR: &'static str =
@@ -68,10 +73,57 @@ async fn sync(krate_req: web::Path<CrateReq>) -> HttpResponse {
     }
 }
 
+fn is_not_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| entry.depth() == 0 || !s.starts_with("."))
+        .unwrap_or(false)
+}
+
+async fn sync_by_each_line(entry: &DirEntry) -> Result<(), std::io::Error> {
+    let input = File::open(entry.path())?;
+    debug!("begin to sync file: {}", entry.path().display());
+    let buffered = BufReader::new(input);
+    let ddl = Instant::now().add(Duration::from_secs(60));
+    let (tx, rx) = async_channel::unbounded();
+    for i in 0..10 {
+        let worker_rx = rx.clone();
+        tokio::spawn(async move {
+            while let Ok(krate) = worker_rx.recv().await {
+                debug!("[worker#{}]start to sync {:?}", i, krate);
+                match Crate::create(krate).await {
+                    Ok(_) => (),
+                    Err(e) => error!("{}", e),
+                };
+            }
+        });
+    }
+    let crates: Vec<CrateReq> = buffered.lines()
+        .filter_map(|r| r.ok())
+        .filter_map(|l| serde_json::from_str(l.as_str()).ok())
+        .collect();
+    for krate in crates {
+        if let Err(e) = tx.send(krate).await {
+            error!("{}", e);
+        }
+    }
+    tokio::time::sleep_until(ddl).await;
+    Ok(())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     log4rs::init_file("config/log4rs.yml", Default::default()).unwrap();
     dotenv::dotenv().ok();
+    let matches = CliApp::new("crates.io sync")
+        .arg(
+            Arg::with_name("sync")
+                .short("s")
+                .long("sync-all")
+                .takes_value(false)
+                .help("sync all crate by index"),
+        ).get_matches();
     let (tx, rx) = async_channel::unbounded();
     for i in 0..10 {
         let worker_rx = rx.clone();
@@ -116,6 +168,23 @@ async fn main() -> std::io::Result<()> {
             tokio::time::sleep_until(ddl).await;
         }
     });
+    if matches.is_present("sync") {
+        debug!("starting sync all crate.io's crates by index");
+        tokio::spawn(
+            async move {
+                for x in WalkDir::new(GIT_INDEX_DIR.to_string())
+                    .into_iter()
+                    .filter_entry(|e| is_not_hidden(e))
+                    .filter_map(|v| v.ok())
+                    .filter(|entry| entry.metadata().ok().unwrap().is_file()) {
+                    sync_by_each_line(&x).await
+                        .expect(format!("failed to sync {}", x.path().display()).as_str())
+
+                }
+
+            }
+        );
+    }
     let server_url = env::var("SERVER_URL").expect("SERVER_URL is not set in .env file");
     let server = HttpServer::new(|| App::new().wrap(Logger::default()).service(sync))
         .bind(server_url)?
