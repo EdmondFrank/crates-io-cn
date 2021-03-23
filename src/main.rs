a851304f4556110c95874fdd3c6f979a51ef8dbc
EdmondFrank <Edmondfrank@hotmail.com>
2021-03-23 18:22:55 +0800

    feat(rust): support web && cli search api

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ extern crate log;
 extern crate lazy_static;
 
 use actix_web::middleware::Logger;
-use actix_web::{get, web, App, HttpResponse, HttpServer};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Error};
 use std::collections::HashMap;
 use std::env;
+
+use serde_json::json;
+use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 use tokio::sync::{mpsc::unbounded_channel, RwLock};
 
@@ -22,6 +25,8 @@ mod upyun;
 mod bos;
 #[cfg(feature = "sync")]
 use clap::{App as CliApp, Arg};
+#[cfg(feature = "search")]
+use elasticsearch::{Elasticsearch, auth::Credentials, SearchParts, http::transport::{TransportBuilder, SingleNodeConnectionPool, Transport}};
 #[cfg(all(feature = "systemd-integration", target_os = "linux"))]
 mod systemd;
 mod easy_git;
@@ -34,11 +39,25 @@ use tokio::time::{Duration, Instant};
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use walkdir::{WalkDir, DirEntry};
+use url::Url;
+use serde_json::Value;
+
+#[cfg(feature = "search")]
+lazy_static! {
+    static ref ELASTIC_URL: &'static str =
+        Box::leak(env::var("ELASTIC_URL").unwrap().into_boxed_str());
+    static ref ELASTIC_USER: &'static str =
+        Box::leak(env::var("ELASTIC_USER").unwrap().into_boxed_str());
+    static ref ELASTIC_PASS: &'static str =
+        Box::leak(env::var("ELASTIC_PASS").unwrap().into_boxed_str());
+    static ref CRATES_INDEX: &'static str =
+        Box::leak(env::var("CRATES_INDEX").unwrap().into_boxed_str());
+}
 
 lazy_static! {
-    static ref GIT_INDEX_DIR: &'static str =
-        Box::leak(env::var("GIT_INDEX_DIR").unwrap().into_boxed_str());
+    static ref GIT_INDEX_DIR: &'static str = Box::leak(env::var("GIT_INDEX_DIR").unwrap().into_boxed_str());
     static ref DL_FORMAT: &'static str = Box::leak(env::var("DL_FORMAT").unwrap().into_boxed_str());
+    static ref API_FORMAT: &'static str = Box::leak(env::var("API_FORMAT").unwrap().into_boxed_str());
     static ref ACTIVE_DOWNLOADS: Arc<RwLock<HashMap<CrateReq, Arc<Crate>>>> =
         Arc::new(RwLock::new(HashMap::new()));
 }
@@ -71,6 +90,76 @@ async fn sync(krate_req: web::Path<CrateReq>) -> HttpResponse {
                 .streaming(rx)
         }
     }
+}
+
+#[cfg(feature = "search")]
+#[derive(Deserialize,Serialize)]
+struct TotalCrates {
+    total: u64,
+}
+
+#[cfg(feature = "search")]
+#[derive(Deserialize,Serialize)]
+struct Crates {
+    crates: Vec<CratePackage>,
+    meta: TotalCrates,
+}
+
+#[cfg(feature = "search")]
+#[derive(Deserialize,Serialize)]
+pub struct CratePackage {
+    pub name: String,
+    pub description: Option<String>,
+    pub max_version: String,
+}
+
+#[cfg(feature = "search")]
+#[derive(Deserialize, Debug)]
+struct SearchParams {
+    q: String,
+    per_page: Option<u32>,
+    page: Option<u32>,
+}
+
+#[cfg(feature = "search")]
+#[get("/api/v1/crates")]
+async fn search(params: web::Query<SearchParams>) -> Result<HttpResponse, Error> {
+    format!("Welcome {:?}!", params);
+    let mut crates = Vec::new();
+    let response = Elasticsearch::new(
+        if let (
+            Ok(user),
+            Ok(pass),
+        ) = (ELASTIC_USER.parse(), ELASTIC_PASS.parse()) {
+            let url = ELASTIC_URL.to_string();
+            let credentials = Credentials::Basic(user, pass);
+            let conn_pool = SingleNodeConnectionPool::new(Url::parse(&url).unwrap());
+            TransportBuilder::new(conn_pool).auth(credentials).build().unwrap()
+        } else {
+            Transport::single_node(&ELASTIC_URL).unwrap()
+        }
+    ).search(SearchParts::Index(&[&CRATES_INDEX]))
+        .body(json!({
+            "query": {
+                "match": {
+                    "name": params.q
+                }
+            },
+            "size": params.per_page.unwrap_or(10),
+            "from": params.page.unwrap_or(0),
+        }))
+        .send()
+        .await.expect("failed to connect elastic");
+    let response_body = response.json::<Value>().await.expect("failed to parse elastic result");
+    let total = response_body["hits"]["total"]["value"].as_u64().unwrap();
+    for hit in response_body["hits"]["hits"].as_array().unwrap() {
+        crates.push(CratePackage {
+            name: hit["_source"]["name"].to_string(),
+            description: Option::from(hit["_id"].to_string()),
+            max_version: hit["_source"]["vers"].to_string()
+        });
+    }
+    Ok(HttpResponse::Ok().json(&Crates { crates, meta: TotalCrates { total, }, }))
 }
 
 fn is_not_hidden(entry: &DirEntry) -> bool {
@@ -142,10 +231,10 @@ async fn main() -> std::io::Result<()> {
             GIT_INDEX_DIR.deref(),
             &Config {
                 dl: DL_FORMAT.to_string(),
+                api: API_FORMAT.to_string(),
                 ..Default::default()
             },
-        )
-        .unwrap();
+        ).unwrap();
         loop {
             let ddl = Instant::now().add(Duration::from_secs(300));
             info!("next update will on {:?}, exec git update now", ddl);
@@ -177,16 +266,19 @@ async fn main() -> std::io::Result<()> {
                     .filter_entry(|e| is_not_hidden(e))
                     .filter_map(|v| v.ok())
                     .filter(|entry| entry.metadata().ok().unwrap().is_file()) {
-                    sync_by_each_line(&x).await
-                        .expect(format!("failed to sync {}", x.path().display()).as_str())
+                        sync_by_each_line(&x).await
+                            .expect(format!("failed to sync {}", x.path().display()).as_str())
 
-                }
+                    }
 
             }
         );
     }
     let server_url = env::var("SERVER_URL").expect("SERVER_URL is not set in .env file");
-    let server = HttpServer::new(|| App::new().wrap(Logger::default()).service(sync))
+    let server = HttpServer::new(|| App::new().wrap(Logger::default())
+                                 .service(sync)
+                                 .service(search)
+    )
         .bind(server_url)?
         .run();
     #[cfg(all(feature = "systemd", target_os = "linux"))]
